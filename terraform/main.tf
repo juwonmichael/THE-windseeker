c866e758b1b5486d4caf72642e95b6965705adc0
juwonmichael <juanmichael179@gmail.com>
2025-01-19 11:42:00 +0000

    fix: clean up terraform configuration and workflow files

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,6 +68,28 @@ resource "aws_iam_user_policy" "ci_policy" {
         Effect = "Allow"
         Action = ["ecr:GetAuthorizationToken"]
         Resource = ["*"]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          aws_s3_bucket.terraform_state.arn,
+          "${aws_s3_bucket.terraform_state.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = [aws_dynamodb_table.terraform_state_lock.arn]
       }
     ]
   })
@@ -76,4 +98,21 @@ resource "aws_iam_user_policy" "ci_policy" {
 # Create access keys for the CI user
 resource "aws_iam_access_key" "ci_user_key" {
   user = aws_iam_user.ci_user.name
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "the-windseeker-terraform-state-${random_string.bucket_suffix.result}"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
+# Add this at the top of your terraform configuration
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
 } 
